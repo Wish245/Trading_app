013c7074ee5d4f2c370aa2347983d9d874999f6b
Wish245 <sankhawishwanath08@gmail.com>
2025-04-14 22:06:32 +0530

    Add user schemas for contact creation and user management

---
+++ Backend-fast-api/app/schemas/users.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr, constr
+from typing import Optional, Annotated
+
+class ContactCreate(BaseModel):
+    contact_type: str
+    contact_value: str
+    is_primary: Optional[bool] = False
+
+class UserCreate(BaseModel):
+    username: str
+    national_id: str
+    password: Annotated[str,constr(min_length=8)]
+    email: EmailStr
+    phone: str
+
+class UserOut(BaseModel):
+    user_id: int
+    username: str
+
+    class Config:
+        orm_mode = True
