ac6f335c0a8333139055bd8d6ce4ef540c834625
Wish245 <sankhawishwanath08@gmail.com>
2025-04-20 20:53:59 +0530

    Refactor Profile component layout and remove unused user details section; add initial ProfileAvatar component and styles.

---
+++ react-frontend/src/components/ProfileAvatar/ProfileAvatar.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import './ProfileAvatar.css';
+
+const ProfileAvatar = ({user_id, avatar_url, username})
--- react-frontend/src/pages/Profile/Profile.jsx
+++ react-frontend/src/pages/Profile/Profile.jsx
@@ -8,15 +8,9 @@ const Profile = () => {
   
 
   return(
-    <div className="main">
+    <div className="profile-container">
       <div className="profile-top">
-        <div className="profile-left">
-          <h4 className="user-details">user details</h4>
-          <div className="avatar-space">
-            <img src={assets.Avatar} alt="Avatar" className="avatar-pic" />
-            <button className="custom-avatar">customize</button>
-          </div>
-        </div>
+        
       </div>
     </div>
   );
