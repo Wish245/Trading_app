2a279265826ee72d6d9752f084a77a9cc6ed5e4a
Wish245 <sankhawishwanath08@gmail.com>
2025-04-24 09:48:39 +0530

    Add user profile API functions and integrate with ProfileLeft component

--- react-frontend/src/components/ProfileLeft/ProfileLeft.jsx
+++ react-frontend/src/components/ProfileLeft/ProfileLeft.jsx
@@ -1,1 +1,24 @@
-import React from "react";
+import React, {useEffect,useState} from 'react';
+import {assets} from '../../assets/assets'
+import { getUserProfile, updateUsername } from '../../api/profile';
+import useUsernameAvailability from '../../hooks/usernameAvailability';
+
+
+export default function profileLeft() {
+    const [editing, setEditing] = useState(false);
+    const [profile, setProfile] = useState({username: '', national_id: ''});
+    const [tempUsername, setTempUsername] = useState('');
+    const {available, checkUsername} = useUsernameAvailability();
+
+    useEffect(()=> {
+        (async() => {
+            try{
+                const data = await getUserProfile();
+                setProfile(data);
+                setTempUsername(data.username);
+            } catch (err) {
+                console.error(err);
+            }
+        })();
+    },[]);
+}
