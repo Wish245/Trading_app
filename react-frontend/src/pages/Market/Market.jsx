6236ea6232c93c70470a39a94cbfa17cae6d35b8
Wish245 <sankhawishwanath08@gmail.com>
2025-05-31 12:59:24 +0530

    Add MyStall and AllStall components to Market page for improved stall management

--- react-frontend/src/pages/Market/Market.jsx
+++ react-frontend/src/pages/Market/Market.jsx
@@ -4,6 +4,8 @@ import React from "react";
 // import NewArrivals from "../../components/NewArrival/NewArrival";
 // import StocksOnSale from "../../components/StocksOnSale/StocksOnSale";
 import { useNavigate } from "react-router-dom";
+import MyStall from "../../components/MyStalls_AllStalls/MyStalls";
+import AllStall from "../../components/MyStalls_AllStalls/AllStall";
 import "./Market.css"
 
 const Market = () => {
@@ -21,6 +23,15 @@ const Market = () => {
       </div>
       <div className="add-stallbtn">
         <button onClick={handleClick} >Build</button>
+      </div>
+      <div className="my-stall-space">
+        <MyStall/>
+      </div>
+      <div className="break-line">
+        <hr className="divider" />
+      </div>
+      <div className="my-stall-space">
+        <AllStall/>
       </div>
     </div>
   );
