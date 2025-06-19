d0bd110fa7583138cea25a7ea6a854434207d9b4
Wish245 <sankhawishwanath08@gmail.com>
2025-06-19 20:02:32 +0530

    This commit will add the virtual environmnet

--- Backend-fast-api/app/models/order.py
+++ Backend-fast-api/app/models/order.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey, Numeric, TIMESTAMP
+from sqlalchemy import Column, Integer, ForeignKey, Numeric, TIMESTAMP, String
 from sqlalchemy.sql import func
 from app.db.base import Base
 
@@ -7,7 +7,7 @@ class Order(Base):
     order_id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
     order_date = Column(TIMESTAMP, server_default=func.now())
-
+    order_status_id = Column(Integer, ForeignKey("order_statuses.order_status_id", ondelete="CASCADE"), nullable=False)
 class OrderItem(Base):
     __tablename__ = "order_items"
     order_item_id = Column(Integer, primary_key=True)
@@ -15,3 +15,8 @@ class OrderItem(Base):
     stock_id = Column(Integer, ForeignKey("stock.stock_id", ondelete="CASCADE"), nullable=False)
     quantity = Column(Integer, nullable=False)
     price_at_order = Column(Numeric(10, 2), nullable=False)
+
+class OrderStatuses (Base):
+    __tablename__ = "order_statuses"
+    order_status_id = Column(Integer, primary_key=True)
+    status_name = Column(String(50), nullable=False, unique=True)
