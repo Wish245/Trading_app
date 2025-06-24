2470ff51abc9f28dafa799f57da66f69f79f4e19
Wish245 <sankhawishwanath08@gmail.com>
2025-06-24 23:35:52 +0530

    feat: Enhance OrderForm with order creation and payment handling

--- react-frontend/src/components/OrderForm/OrderForm.jsx
+++ react-frontend/src/components/OrderForm/OrderForm.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
-import * as OrderAPI from "../../api/order"
+import * as OrderAPI from "../../api/order";
+import * as StockAPI from "../../api/stock";
 import { useNavigate, useParams } from "react-router-dom";
 import "./OrderForm.css";
 
@@ -7,6 +8,8 @@ const OrderForm = () => {
     const [quantity, setQuantity] = useState("");
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [isOpenOrder, setOpenOrder] = useState(false);
+    const [isOpenPayement, setOpenPayment] = useState(false);
     const navigate = useNavigate();
     const { stock_id } = useParams();
 
@@ -18,12 +21,40 @@ const OrderForm = () => {
         try {
             const response = await OrderAPI.createOrder(stock_id, quantity, 1, 1);
             console.log("Order created:",response.data);
-            navigate(`/order`);
+            setOpenOrder(true);
+            setTimeout(() => {
+                navigate(`/order`);
+            },2000)
         } catch (err) {
             console.error(err);
             setError("Error creating order");
         } finally {
             setLoading(false);
+            setOpenOrder(false);
         }
+    }
+    const handlePayment = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await OrderAPI.createOrder(stock_id, quantity, 1, 1);
+            console.log("Order created:",response.data);
+            console.log("Proceeding to payment...");
+            setOpenPayment(true);
+            setTimeout(() => {
+                navigate(`/order`);
+            },2000)
+        } catch (err) {
+            console.error(err);
+            setError("Error creating order");
+        } finally {
+            setLoading(false);
+            setOpenPayment(false);
+        }
+    }
+
+    const onCancel = () => {
+        navigate(`/stock/${stock_id}`);
     }
 }
