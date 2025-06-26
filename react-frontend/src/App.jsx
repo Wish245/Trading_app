f09eed1a0f9e6c2191db4ae76ed870d452f3e072
Wish245 <sankhawishwanath08@gmail.com>
2025-06-26 22:47:22 +0530

    orderform

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -12,6 +12,7 @@ import SignUp from './pages/SignUp/SignUp'
 import AddStall from './components/AddStall/AddStall'
 import StockForm from './components/StockForm/StockForm'
 import Stock from './components/Stock/Stock'
+import OrderForm from './components/OrderForm/OrderForm'
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
           <Route path='/createStall' element={<AddStall/>}/>
           <Route path='/stock-form/:stall_id' element={<StockForm/>}/>
           <Route path='/stock/:stock_id' element={<Stock/>}/>
+          <Route path='/order-form/:stock_id' element={<OrderForm/>}/>
         </Routes>
       </div>
       <Footer />
